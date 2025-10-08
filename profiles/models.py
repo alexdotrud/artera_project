e8e3d17400803e903985a6f853e7c5c52e9eca38
alexdotrud <rudenko530@gmail.com>
2025-10-08 13:08:45 +0200

    Create profiles app, add it to settings/url, add Profile model

---
+++ profiles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100, blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    address_line_delivery = models.CharField(max_length=255, blank=True)
+    address_line_living = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    postal_code = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
