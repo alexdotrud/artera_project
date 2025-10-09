b63dc24abdd4a87951fef6530bf93f2d7098f507
alexdotrud <rudenko530@gmail.com>
2025-10-09 14:00:36 +0200

    Create Search model for the homepage

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class SearchDocument(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    url = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
