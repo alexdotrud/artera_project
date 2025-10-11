23fb3a8a40ce695321824a1779d5b524ef238db8
alexdotrud <rudenko530@gmail.com>
2025-10-11 12:59:52 +0200

    Add remove from bag function'

--- bag/views.py
+++ bag/views.py
@@ -67,7 +67,7 @@ def adjust_bag(request, item_id):
                 if not bag[item_id]["items_by_size"]:
                     bag.pop(item_id, None)
                 messages.success(request, f"Removed size {size.upper()} {artwork.name} from your bag")
-                
+
         if item_id in bag:
             if quantity > 0:
                 bag[item_id] = quantity
@@ -79,3 +79,24 @@ def adjust_bag(request, item_id):
     request.session["bag"] = bag
     return redirect(reverse("bag:view_bag"))
 
+def remove_from_bag(request, item_id):
+    """Remove the item (or specific size) from the bag."""
+    artwork = get_object_or_404(Artwork, pk=item_id)
+
+    try:
+        size = request.POST.get("artwork_size")
+        bag = request.session.get("bag", {})
+
+        # remove just this size
+        if item_id in bag and "items_by_size" in bag[item_id]:
+            bag[item_id]["items_by_size"].pop(size, None)
+            if not bag[item_id]["items_by_size"]:
+                bag.pop(item_id, None)
+        messages.success(request, f"Removed size {size.upper()} {artwork.name} from your bag")
+
+        request.session["bag"] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f"Error removing item: {e}")
+        return HttpResponse(status=500)
