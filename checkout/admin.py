582b742fc02ed245c5a5ff6c2570e7ff7f9707d1
alexdotrud <rudenko530@gmail.com>
2025-10-12 18:35:20 +0200

    Create an OrderItem class with all nessesary fields

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    # calculated/auto fields should be read-only
+    readonly_fields = (
+        'order_number',
+        'date',
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+
+    # field layout in the admin detail page
+    fields = (
+        'order_number',
+        'date',
+
+        'full_name',
+        'email',
+        'phone_number',
+
+        'country',
+        'postcode',
+        'town_or_city',
+        'street_address1',
+        'street_address2',
+        'county',
+
+        'delivery_cost',
+        'order_total',
+        'grand_total',
+    )
+
+    # columns in the list view
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'order_total',
+        'delivery_cost',
+        'grand_total',
+    )
+
+    ordering = ('-date',)
