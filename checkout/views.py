f54a694874becfc2bad5f8f5a5bddd897577bc68
alexdotrud <rudenko530@gmail.com>
2025-10-13 16:31:11 +0200

    Install stripe package, define stripe total in checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from bag.contexts import bag_contents
+import stripe
 
 def checkout(request):
     bag = request.session.get('bag', {})
@@ -8,6 +11,9 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('all_artworks'))
     
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
