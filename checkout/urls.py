2230d98e060c0727c831ec245f2c55a0b5e7e8a1
alexdotrud <rudenko530@gmail.com>
2025-10-14 14:54:40 +0200

    Test all created webhooks -works properly

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from . import webhooks
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('success/<order_number>/', views.checkout_success, name='checkout_success'),
+    path('webhook/', webhooks.webhook, name='stripe_webhook'),
 ]
