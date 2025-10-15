12eca4ff48113f212d8a2c3f23d97327f41bbef5
alexdotrud <rudenko530@gmail.com>
2025-10-15 19:44:38 +0200

    Create profile view to updating personal info

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,32 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Prefetch, Count
+from django.contrib import messages
 
 from checkout.models import OrderItem, Order
+from .forms import ProfileForm
+from .models import Profile
 
+@login_required
+def profile(request):
+    profile, _ = Profile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated.")
+            return redirect('profile')
+        else:
+            messages.error(request, "Please fix the errors below.")
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request, 'profiles/profile.html', {
+        'form': form,
+        'user_obj': request.user,
+    })
 
 @login_required
 def library(request):
