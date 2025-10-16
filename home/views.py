0b191ab5a4e8fa7d1b302019bccb3ea2f677fc4a
alexdotrud <rudenko530@gmail.com>
2025-10-16 19:55:45 +0200

    Add some little style changes for home/shop pages

--- home/views.py
+++ home/views.py
@@ -13,22 +13,5 @@ def artwork_search(request):
     if not q:
         messages.error(request, "You didn't enter any search criteria!")
         return redirect("/")
-
-    # Split into words for multi-word search
-    words = [word.strip() for word in q.split() if word.strip()]
-    if not words:
-        messages.error(request, "Invalid search query!")
-        return redirect("/")
-    
-    q_objects = Q()
-    for word in words:
-        word_q = Q(name__icontains=word) | Q(description__icontains=word)
-        q_objects &= word_q
-
-    results = Artwork.objects.filter(q_objects).distinct().order_by('-created_at')
-
-    return render(request, "home/search_results.html", {
-        "q": q,
-        "results": results,
-        "SIZE_SURCHARGE": SIZE_SURCHARGE,
-    })
+    else:
+        return render(request, "shop/list.html")
