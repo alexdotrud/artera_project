3d11b75edadb1198e7db9d23526985a7f31ca8af
alexdotrud <rudenko530@gmail.com>
2025-10-16 20:27:37 +0200

    Create signals to make updates email primary

--- profiles/signals.py
+++ profiles/signals.py
@@ -3,8 +3,22 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 from .models import Profile
+from allauth.account.signals import email_confirmed
+from allauth.account.models import EmailAddress
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(email_confirmed)
+def make_confirmed_email_primary(request, email_address, **kwargs):
+    """When a user confirms a new email, make it primary and sync to user.email."""
+    user = email_address.user
+    # Make this the only primary
+    EmailAddress.objects.filter(user=user).update(primary=False)
+    email_address.primary = True
+    email_address.save(update_fields=['primary'])
+
+    user.email = email_address.email
+    user.save(update_fields=['email'])
