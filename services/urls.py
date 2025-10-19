c200cc7f8ed741236c1d0d662031c9404c7d72bc
alexdotrud <rudenko530@gmail.com>
2025-10-19 13:22:45 +0200

    Create views for sevices, it will all contain submiting forms

--- services/urls.py
+++ services/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('request', views.artwork_request, name='request'),
+    path("requests/<int:pk>/", views.request_detail, name="request_detail"),
     path('offer', views.offer_request, name='offer'),
+    path("offers/success/", views.offer_success, name="offer_success"),
 ]
