5c48133e03e67860a79b98134d4bba6e0439e84e
alexdotrud <rudenko530@gmail.com>
2025-10-19 14:45:44 +0200

    add actions to request admin fiel

--- services/admin.py
+++ services/admin.py
@@ -6,6 +6,8 @@ class ArtworkRequestAdmin(admin.ModelAdmin):
     list_display = ("id", "title", "user", "status", "created_at")
     list_filter = ("status", "created_at")
     search_fields = ("title", "description", "user__username")
+    list_editable = ("status",)
+    actions = ["mark_in_review", "mark_accepted", "mark_rejected"]
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
