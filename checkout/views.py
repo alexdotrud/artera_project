bf189c4651b2aa0a7d2d709eee7b2800ff47847a
alexdotrud <rudenko530@gmail.com>
2025-10-20 21:16:03 +0200

    Iprove the cache checkout data function

--- checkout/views.py
+++ checkout/views.py
@@ -19,13 +19,12 @@ def cache_checkout_data(request):
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
             'bag': json.dumps(request.session.get('bag', {})),
-            'save_info': request.POST.get('save_info'),
+            'save_info': request.POST.get('save-info'),  # <-- fix here
             'username': getattr(request.user, 'username', ''),
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
+        messages.error(request, 'Sorry, your payment cannot be processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
     
 def checkout(request):
