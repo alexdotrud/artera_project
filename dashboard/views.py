e4d6837f1ab897222799cc6e3c71631d2dcf235d
alexdotrud <rudenko530@gmail.com>
2025-10-23 10:46:49 +0200

    Add artwork edit function to make changes to existing posts and delete function

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.http import require_POST
 from django.contrib import messages
@@ -28,5 +28,25 @@ def artwork_create(request):
         messages.success(request, "Artwork created.")
     else:
         messages.error(request, "Please fix the errors and try again.")
-    return redirect("dashboard")
+    return redirect("dashboard:dashboar")
 
+@staff_member_required
+def artwork_edit(request, pk):
+    art = get_object_or_404(Artwork, pk=pk)
+    if request.method == "POST":
+        form = ArtworkForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Artwork updated.")
+            return redirect("dashboard:dashboard")
+        messages.error(request, "Please fix the errors and try again.")
+    else:
+        form = ArtworkForm(instance=art)
+    return render(request, "dashboard/artwork_form.html", {"form": form, "art": art})
+
+@staff_member_required
+@require_POST
+def artwork_delete(request, pk):
+    get_object_or_404(Artwork, pk=pk).delete()
+    messages.success(request, "Artwork deleted.")
+    return redirect("dashboard:dashboard")
