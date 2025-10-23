08d7e3a9e747b24e82a2909bd77c67e3a3a3dd13
alexdotrud <rudenko530@gmail.com>
2025-10-23 10:44:46 +0200

    Create function artwork create to add a new artwork

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.decorators.http import require_POST
+from django.contrib import messages
 
 from shop.models import Artwork
 from services.models import ArtworkRequest, Offer
@@ -16,3 +18,15 @@ def dashboard(request):
         "offers": offers,
         "artwork_form": ArtworkForm(),
     })
+
+@staff_member_required
+@require_POST
+def artwork_create(request):
+    form = ArtworkForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Artwork created.")
+    else:
+        messages.error(request, "Please fix the errors and try again.")
+    return redirect("dashboard")
+
