2ec9c27b0cc9211c9f5d76874379ea34bc192c14
alexdotrud <rudenko530@gmail.com>
2025-11-09 14:58:02 +0100

    Create custom verifivation emails for registred users

--- artera/settings.py
+++ artera/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Security settings
-DEBUG = os.environ.get("DEBUG") == "True"
+DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
