484bbd8440063345a805c30ee81e16885036201d
alexdotrud <rudenko530@gmail.com>
2025-11-16 12:03:13 +0100

    Make category images clickable as a link

--- artera/settings.py
+++ artera/settings.py
@@ -7,7 +7,7 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security settings
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
