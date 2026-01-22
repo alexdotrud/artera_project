09dbadd6800a8d5c0d1fe9c86c75450b999bff73
alexdotrud <rudenko530@gmail.com>
2026-01-22 12:41:23 +0100

    fix db to use prod db on heroku with debug=True

--- artera/settings.py
+++ artera/settings.py
@@ -129,7 +129,7 @@ else:
 
 
 # Database
-if DEBUG == False and 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
